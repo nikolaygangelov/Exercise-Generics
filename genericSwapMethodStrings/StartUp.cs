d6b5b2b3deb96fcedef1f34d2c1d94251c684d5b
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-02-08 00:00:43 +0200

    Add files via upload

---
+++ genericSwapMethodStrings/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace genericSwapMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStrings = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                string item = Console.ReadLine();
+                box.Add(item);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int indexToswap1 = indexes[0];
+            int indexToswap2 = indexes[1];
+            box.Swap(indexToswap1, indexToswap2);
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
