3186f9d2b82c465bbcd2a77fe744b6d95a7e2dc4
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-02-07 14:33:28 +0200

    Add files via upload

---
+++ genericBoxOfString/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace genericBoxOfString
+{
+    public class Box <T>
+    {
+        private List<T> items;
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ genericBoxOfString/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace genericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int numberOfStrings = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                string item = Console.ReadLine();
+                box.Add(item);
+            }
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
