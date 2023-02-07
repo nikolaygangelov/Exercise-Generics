6c5fa68dd4300408fb951c4bd4c32a2dd9bf650a
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-02-08 00:06:58 +0200

    Add files via upload

---
+++ genericCountMethodStrings/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace genericCountMethodStrings
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            (items[firstIndex], items[secondIndex]) = (items[secondIndex], items[firstIndex]);
+        }
+
+        public int Compare(T compairedElement) 
+        {
+            int count = 0;
+            foreach (T item in items)
+            {
+                if (item.CompareTo(compairedElement) == 1)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ genericCountMethodStrings/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace genericCountMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new Box<string>();
+            int numberOfElements = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                string element = Console.ReadLine();
+                box.Add(element);
+            }
+            string compairedElement = Console.ReadLine();
+            Console.WriteLine(box.Compare(compairedElement));
+
+        }
+    }
+}
