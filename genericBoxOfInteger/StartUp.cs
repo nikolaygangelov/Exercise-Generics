c9b98532dab619a93bd7f5b2f9ed28f8ba0054d4
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-02-07 23:59:12 +0200

    Add files via upload

---
+++ genericBoxOfInteger/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace genericBoxOfInteger
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStrings = int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>();
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                int item = int.Parse(Console.ReadLine());
+                box.Add(item);
+            }
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
