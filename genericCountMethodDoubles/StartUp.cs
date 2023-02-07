cef1e1bf0a3fc78e2aa4cd11ab27c5e8eaee8999
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-02-08 00:20:22 +0200

    Add files via upload

---
+++ genericCountMethodDoubles/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace genericCountMethodDoubles
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<double> box = new Box<double>();
+            int numberOfElements = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                double element = double.Parse(Console.ReadLine());
+                box.Add(element);
+            }
+            double compairedElement = double.Parse(Console.ReadLine());
+            Console.WriteLine(box.Compare(compairedElement));
+        }
+    }
+}
